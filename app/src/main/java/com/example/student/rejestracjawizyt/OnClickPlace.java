fe47b7a47fdd791765b0f95d412147eb10c63bfc
unknown <student@IBLAB19N-06.ib.polsl.pl>
2018-01-31 14:45:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/student/rejestracjawizyt/OnClickPlace.java
@@ -0,0 +1,11 @@
+package com.example.student.rejestracjawizyt;
+
+/**
+ * Created by student on 2018-01-31.
+ */
+import android.view.View;
+
+public interface OnClickPlace {
+    void onClickPlace(View view, int position);
+
+}
